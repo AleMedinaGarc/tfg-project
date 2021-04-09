e18b3368a1511c8f5c4d9cb442d6e4d0e81763ae
Alejandro Medina Garcia <alu0100905885@ull.edu.es>
2021-04-09 13:49:29 +0100

    api connection working

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -1,58 +1,54 @@
 import React, { useState, useEffect } from "react";
-import { ActivityIndicator, View, Text } from "react-native";
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import { fonts } from "../styles/fonts"
 
 import { getJsonConfigConst } from "../components/DataStream"
 
 
 export default function ProductScreen({ route, navigation }) {
-    // codigo de barras
     const { barCode } = route.params;
     const [isLoading, setLoading] = useState(true);
-    // const [name, setName]
+    const [data, setData] = useState([]);
     getJsonConfigConst();
 
-    function checkName(json) {
-        if (json.product_name != "")
-            return json.product_name;
+    function checkName() {
+        if (data.status == 0)
+            return "No está escaneando un alimento";
+        if (data.product.product_name != "")
+            return data.product.product_name;
         else
-            if (json.name_en != "")
-                return json.name_en;
+            if (data.product.product_name_es != "")
+                return data.product.name_en;
             else
-                if (json.product_name_en != "")
-                    return json.product_name_en;
+                if (data.product.name_en != "")
+                    return data.product.product_name_en;
                 else
-                    if (json.generic_name != "")
-                        return json.generic_name;
+                    if (data.product.product_name_en != "")
+                        return data.product.generic_name;
                     else
-                        if (json.product_name_fr != "")
-                            return json.product_name_fr;
-                        else
-                            return "noNamed";
+                        return "noNamed";
     }
 
-    const getProduct = async () => {
-        try {
-            let response = await fetch(
-                `https://world.openfoodfacts.org/api/v0/product/${barCode}.json`
-            ).finally(() => setLoading(false));
-            let json = await response.json();
-            return checkName(json);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+
 
     useEffect(() => {
-        getProduct();
-    })
+        fetch(`https://world.openfoodfacts.org/api/v0/product/${barCode}.json`)
+            .then((response) => response.json())
+            .then((json) => setData(json))
+            .catch((error) => console.error(error))
+            .finally(() => setLoading(false));
+    }, []);
 
     return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center", paddingBottom: "3%" }}>
-            {isLoading ? <ActivityIndicator /> : (
-                <Text style={fonts.blackFont}>{getProduct}</Text>
-            )}
-          
+        <View style={{ flex: 1, padding: 24 }}>
+            {isLoading ?
+                <ActivityIndicator size={70} color="#9ad1d4" style={{ flex: 1, alignItems: "center", justifyContent: "center" }} /> : (
+                    <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
+                        <Text>{checkName()}</Text>
+                    </View>
+                )}
         </View>
     );
 }
+
+<ActivityIndicator size={70} color="#9ad1d4" style={{ flex: 1, alignItems: "center", justifyContent: "center" }} />
