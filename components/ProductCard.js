bc17c881d351a81ba5d83998cf1ea797e55a4646
Alejandro Medina <alu0100905885@ull.edu.es>
2021-04-13 23:39:17 +0100

    -m

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -11,13 +11,15 @@ import { Title, Button } from "react-native-paper";
 export default function ProductCard({ data, config, navigation }) {
     const [backColor, setBackColor] = useState("#FFFFF");
     const [edible, setEdible] = useState(true);
-    //let allergensFound = [];
     const [allergensFound, setAllergensFound] = useState();
-
+    const [isValid, setIsValid] = useState(true)
 
     useEffect(() => {
         if (data == null || data == undefined) return;
-        if (data.status == 0) return <Text>No está escaneando un alimento o no se encuentra en la base de datos.</Text>;
+        if (data.status == 0) {
+            setIsValid(false);
+            return;
+        }
         let mergeAllergens = data.product.allergens_tags.concat(data.product.traces_tags)
 
         for (let tag of mergeAllergens)
@@ -39,8 +41,7 @@ export default function ProductCard({ data, config, navigation }) {
     });
 
     function checkName() {
-        if (data == null) return;
-        if (data.status == 0) return <Text>No está escaneando un alimento o no se encuentra en la base de datos.</Text>;
+        if (data == null || data.status == 0) return;
 
         if (data.product.product_name_es != "")
             return data.product.product_name;
@@ -58,8 +59,8 @@ export default function ProductCard({ data, config, navigation }) {
     }
 
     function checkImage() {
-        if (data == null) return;
-        if (data.status == 0) return <Text>No está escaneando un alimento o no se encuentra en la base de datos.</Text>;
+        if (data == null || data.status == 0) return;
+
         if (data.product.image_front_url != "")
             return data.product.image_front_url;
         else
@@ -69,38 +70,53 @@ export default function ProductCard({ data, config, navigation }) {
     function renderEdible() {
         if (data == null) return;
         if (data.status == 0) return <Text>No está escaneando un alimento o no se encuentra en la base de datos.</Text>;
-        if (edible == true) {
-            if (data.product.unique_scans_n <= 20) {
-                return <Text style={fonts.blackFont}><Y>{`Apto, pero tiene un total de ${data.product.unique_scans_n} escaneos totales.`}</Y></Text> //en naranja
-            } else {
+        if (edible == true)
+            if (data.product.unique_scans_n <= 20)
+                return <Text style={fonts.blackFont}><Y>{`Apto, pero tiene un total de ${data.product.unique_scans_n} escaneos totales.`}</Y></Text>
+            else
                 return <Text style={fonts.blackFont}><G>Apto para consumo</G></Text>
-            }
-        } else {
+        else {
             return (
                 <View style={{ alignItems: "center", justifyContent: "center" }}>
                     <Text style={fonts.blackFont}><R>No apto para consumo</R></Text>
                     <Text style={fonts.blackFont}>Contiene: {allergensFound}</Text>
                 </View>
             )
-
         }
-
     }
+
 
     return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center", width: "100%", height: "100%", backgroundColor: `${backColor}` }}>
-            <View style={styles.blankCard}>
-                <Image source={{ uri: `${checkImage()}` }} style={styles.cardImage} />
-                <Title>{checkName()}</Title>
-                {renderEdible()}
-                <Button
-                mode="contained" color="#80ced7"
-                onPress={() => navigation.pop()}
-                style={styles.buttonCustom}
-            >
-                <Text style={styles.buttonText}>Escanear de nuevo</Text>
-            </Button>
-            </View>
+            {isValid ?
+                (
+                    <View style={styles.blankCard}>
+                        <Image source={{ uri: `${checkImage()}` }} style={styles.cardImage} />
+                        <Title>{checkName()}</Title>
+                        {renderEdible()}
+                        <Button
+                            mode="contained" color="#80ced7"
+                            onPress={() => navigation.pop()}
+                            style={styles.buttonCustom}
+                        >
+                            <Text style={styles.buttonText}>Escanear de nuevo</Text>
+                        </Button>
+                    </View>
+                ) :
+                (
+                    <View style={styles.blankCardError} >
+                        <Text style={fonts.blackFont} >No está escaneando un alimento o no se encuentra en la base de datos.</Text>
+                        <Button
+                            mode="contained" color="#80ced7"
+                            onPress={() => navigation.pop()}
+                            style={styles.buttonCustom}
+                        >
+                            <Text style={styles.buttonText}>Escanear de nuevo</Text>
+                        </Button>
+                    </View>
+                )}
+
+
         </View>
     );
 }
@@ -130,11 +146,31 @@ const styles = StyleSheet.create({
         marginLeft: 0,
         marginRight: 0,
         justifyContent: "center",
-     },
-     buttonText: {
+    },
+    buttonText: {
         textAlign: "center",
         fontWeight: "bold",
         height: "100%",
         width: "100%",
+    },
+    errorMesage: {
+        alignItems: "center",
+        marginTop: "50%",
+        marginBottom: "10%",
+        width: 200,
+        justifyContent: "center",
+
+    },
+    blankCardError: {
+        borderRadius: 10,
+        borderColor: "#ff6666",
+        borderWidth: 3,
+        flex: 1,
+        alignItems: "center",
+        marginTop: "50%",
+        marginBottom: "60%",
+        width: "80%",
+        height: "80%",
+        backgroundColor: "#FFFFFF",
     },
 });
